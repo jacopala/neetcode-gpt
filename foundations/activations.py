a52aa0a9eef9628094563cc2070db4c9047d9807
JacobP <jaco.pala@icloud.com>
2026-07-02 14:37:07 -0500

    Update Activation functions (Math Foundations 2/6)

--- foundations/activations.py
+++ foundations/activations.py
@@ -6,9 +6,9 @@ class Solution:
     
     def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
-        f = 1 / (1 + pow(np.e,-1*z))
+        f = 1 / (1 + np.exp(-z))
         return np.round(f, 5)
 
     def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
-        return np.where(0<z, z, 0)
+        return np.maximum(0, z)
